b258b0250fa0a87b1a49e5d6fcb03c683c9cd67f
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-05-04 16:35:42 +0200

    ex 4

--- src/test/scala/excercises/TestListProperties.scala
+++ src/test/scala/excercises/TestListProperties.scala
@@ -1,7 +1,7 @@
 package excercises
 
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 
 class TestListProperties extends Properties("List"):
   property("Concat is associative") = forAll { (xs: List[Unit], ys: List[Unit], zs: List[Unit]) =>
@@ -10,4 +10,23 @@ class TestListProperties extends Properties("List"):
 
   property("Array and list concat results in a List") = forAll { (xs: List[Unit]) =>
     xs ++ Array[Unit]() == xs
+  }
+
+  property("Mapping with Identity returns the same List") = forAll { (xs: List[Unit]) =>
+    xs.map(identity) == xs
+  }
+
+  property("Composition") = forAll { (xs: List[Int], f: Int => Int, g: Int => Int) =>
+    xs.map(f compose g) == xs.map(g).map(f)
+  }
+
+class TestPalindromes extends Properties("String"):
+  val palindromeGen: Gen[String] =
+    for
+      s <- Gen.alphaStr
+      c <- Gen.alphaChar
+    yield s + c + s.reverse
+
+  property("Palindromeness") = forAll(palindromeGen) { (s: String) =>
+    s == s.reverse
   }
